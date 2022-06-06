5a7b71c7047da77803652b25fbd5a37a2c08f9d4
Quang Bao <hoangquangbao.developer@gmail.com>
2022-06-06 16:47:27 +0700

    Apply Transition

--- ComplexMatchedGeometryEffect/View/Home.swift
+++ ComplexMatchedGeometryEffect/View/Home.swift
@@ -13,6 +13,8 @@ struct Home: View {
     @Namespace var animation
     @State var isExpanded: Bool = false
     @State var expandedProfile: Profile?
+    //SEE ANIMATION HACK VIDEO
+    @State var isLoadExpandedContent: Bool = false
     
     var body: some View {
         NavigationView {
@@ -25,7 +27,7 @@ struct Home: View {
             }
             .navigationTitle("WhatApp")
         }
-//        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
+        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
         .overlay{
             if let expandedProfile = expandedProfile,isExpanded {
                 ExpandedView(profile: expandedProfile)
@@ -43,15 +45,17 @@ struct Home: View {
                         .resizable()
                         .aspectRatio(contentMode: .fill)
                         .frame(width: 45, height: 45)
-                        .clipShape(Circle())
+//                        .clipShape(Circle())
+                        .cornerRadius(0)
                         .opacity(0)
                 } else {
                     Image(profile.profilePicture)
                         .resizable()
                         .aspectRatio(contentMode: .fill)
                         .frame(width: 45, height: 45)
-                        .clipShape(Circle())
+//                        .clipShape(Circle())
                         .matchedGeometryEffect(id: profile.id, in: animation)
+                        .cornerRadius(25)
                 }
             }
             .onTapGesture {
@@ -77,31 +81,53 @@ struct Home: View {
                 .foregroundColor(.gray)
         }
         .padding(.horizontal)
+        .blur(radius: isLoadExpandedContent ? 3 : 0)
+        .disabled(isLoadExpandedContent)
     }
     
     //MARK: Expanded View
     @ViewBuilder
     func ExpandedView(profile: Profile) -> some View {
-        VStack {
-            GeometryReader { proxy in
-                let size = proxy.size
-                
-                Image(profile.profilePicture)
-                    .resizable()
-                    .aspectRatio(contentMode: .fill)
-//1.                    .matchedGeometryEffect(id: profile.id, in: animation)
-                    .frame(width: size.width, height: size.height)
-                //1. IF WE USE CLIP IT WILL CLIP IMAGE FROM TRANSITION
-                    .clipped()
-                //2. IF WE USE AFTER CLIP IT WILL UN POSITION THE VIEW
-//2.                    .matchedGeometryEffect(id: profile.id, in: animation)
+            
+            VStack {
+                GeometryReader { proxy in
+                    let size = proxy.size
+                    
+                    Image(profile.profilePicture)
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+    //1.                    .matchedGeometryEffect(id: profile.id, in: animation)
+                        .frame(width: size.width, height: size.height)
+                    //1. IF WE USE CLIP IT WILL CLIP IMAGE FROM TRANSITION
+                    //TO AVOID IMMEDIATE CLIP RELASE APPLYING CORNER RADIUS
+    //                    .clipped()
+                        .cornerRadius(isLoadExpandedContent ? 0 : size.height)
+                    //2. IF WE USE AFTER CLIP IT WILL UN POSITION THE VIEW
+    //2.                    .matchedGeometryEffect(id: profile.id, in: animation)
+                        .onTapGesture {
+                            withAnimation(.easeInOut(duration: 4)) {
+                                isLoadExpandedContent = false
+                            }
+                            withAnimation(.easeInOut(duration: 4).delay(0.05)) {
+                                isExpanded = false
+                            }
+                        }
+                }
+                //3. WORKAROUND WRAP IT INSIDE GEOMETRY READER AND APPLY BEFORE FRAME
+                //3.
+                .matchedGeometryEffect(id: profile.id, in: animation)
+                .frame(height: 300)
             }
-            //3. WORKAROUND WRAP IT INSIDE GEOMETRY READER AND APPLY BEFORE FRAME
-            //3.
-            .matchedGeometryEffect(id: profile.id, in: animation)
-            .frame(height: 300)
-        }
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            //FOR MORE CLEAN TRANSITION USE TRANSITION WITH OFFSET
+            //FOR MORE ABOUT MATCHED GEOMETRY TRANSITION
+            .transition(.offset(x: 0, y: 1))
+            .onAppear {
+                //DURATION 4 IS FOR TESTING
+                withAnimation(.easeInOut(duration: 4)) {
+                    isLoadExpandedContent = true
+                }
+            }
     }
 }
 
